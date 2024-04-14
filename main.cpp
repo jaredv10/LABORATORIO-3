00257ca8ca1692d65dbedd932e50250c7cea3b53
jaredv10 <160081426+jaredv10@users.noreply.github.com>
2024-04-14 18:08:49 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,19 +6,34 @@ using namespace std;
 
 int main()
 {
-    cout<< "Menu de ejercicios " << endl;
+    cout<< "Seleccione la opcion que deseas ejercutar " << endl;
     cout<< "Ejercicio 1 "<< endl;
     cout<< "Ejercicio 2 "<< endl;
     cout<< "Ejercicio 3 "<< endl;
     cout<< "Ejercicio 4 "<< endl;
     cout<< "Ejercicio 5 "<< endl;
-    cout<< "Seleccione una opcion"<< endl;
+    cout<< "Seleccion: "<< endl;
     int opcion = 0;
     cin>> opcion;
     switch (opcion){
 
     case 1:
         problema1();
+        break;
+    default:
+        cout << "opción no disponible"<< endl;
+        break;
+     case 2:
+        problema2();
+        break;
+    case 3:
+        problema3();
+        break;
+    case 4:
+        problema4();
+        break;
+    case 5:
+        problema5();
         break;
     default:
         cout << "opción no disponible"<< endl;
