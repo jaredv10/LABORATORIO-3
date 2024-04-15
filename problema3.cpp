09790d9e31aec61807eefe97832a707dafc32f83
jaredv10 <160081426+jaredv10@users.noreply.github.com>
2024-04-15 11:46:00 -0500

    Update problema3.cpp

--- problema3.cpp
+++ problema3.cpp
@@ -6,19 +6,35 @@ using namespace std;
 int problema3() {
     Buscartexto buscador;
     string archivoEntrada, archivoSalida, busqueda;
+    char opcion;
 
-    cout << "Ingrese la secuencia a buscar: ";
-    getline(cin, busqueda);
+    do {
+        cout << "\n** MENÚ DE BUSCADOR DE TEXTO **" << endl;
+        cout << "1. Buscar y contar en un archivo" << endl;
+        cout << "2. Salir" << endl;
+        cout << "Seleccione una opción: ";
+        cin >> opcion;
+        cin.ignore();
 
-    cout << "Ingrese el nombre del archivo de entrada: ";
-    getline(cin, archivoEntrada);
+        if (opcion == '1') {
+            cout << "Ingrese la secuencia a buscar: ";
+            getline(cin, busqueda);
 
-    cout << "Ingrese el nombre del archivo de salida: ";
-    getline(cin, archivoSalida);
+            cout << "Ingrese el nombre del archivo de entrada: ";
+            getline(cin, archivoEntrada);
 
-    buscador.buscarYContar(archivoEntrada, busqueda, archivoSalida);
+            cout << "Ingrese el nombre del archivo de salida: ";
+            getline(cin, archivoSalida);
 
-    cout << "Proceso completado. Los resultados se han escrito en " << archivoSalida << ".\n";
+            buscador.buscarYContar(archivoEntrada, busqueda, archivoSalida);
+
+            cout << "Proceso completado. Los resultados se han escrito en " << archivoSalida << ".\n";
+        } else if (opcion == '2') {
+            cout << "Saliendo del programa.\n";
+        } else {
+            cout << "Opción no válida.\n";
+        }
+    } while (opcion != '2');
 
     return 0;
 }
