36229abba449ad9a7425ada159bbc1930f55a804
jaredv10 <160081426+jaredv10@users.noreply.github.com>
2024-04-16 17:16:33 -0500

    Update problema5.cpp

--- problema5.cpp
+++ problema5.cpp
@@ -2,24 +2,26 @@
 #include <fstream>
 #include "pelicula.h"
 
+using namespace std;
+
 #define MAX_PELICULAS 100
 
 Pelicula peliculas[MAX_PELICULAS]; // Arreglo de películas
 int numPeliculas = 0; // Número de películas en el arreglo
 
 void ingresarPelicula() {
-    std::string nombre, director, genero;
+    string nombre, director, genero;
     int anio, horas, minutos, segundos;
-    std::cout << "Ingrese el nombre de la película: ";
-    std::cin >> nombre;
-    std::cout << "Ingrese el director: ";
-    std::cin >> director;
-    std::cout << "Ingrese el año de lanzamiento: ";
-    std::cin >> anio;
-    std::cout << "Ingrese el género: ";
-    std::cin >> genero;
-    std::cout << "Ingrese la duración (horas, minutos y segundos): ";
-    std::cin >> horas >> minutos >> segundos;
+    cout << "Ingrese el nombre de la película: ";
+    cin >> nombre;
+    cout << "Ingrese el director: ";
+    cin >> director;
+    cout << "Ingrese el año de lanzamiento: ";
+    cin >> anio;
+    cout << "Ingrese el género: ";
+    cin >> genero;
+    cout << "Ingrese la duración (horas, minutos y segundos): ";
+    cin >> horas >> minutos >> segundos;
     Duracion duracion(horas, minutos, segundos);
     Pelicula pelicula(nombre, director, anio, genero, duracion);
     peliculas[numPeliculas] = pelicula;
@@ -27,9 +29,9 @@ void ingresarPelicula() {
 }
 
 void eliminarPelicula() {
-    std::string nombre;
-    std::cout << "Ingrese el nombre de la película a eliminar: ";
-    std::cin >> nombre;
+    string nombre;
+    cout << "Ingrese el nombre de la película a eliminar: ";
+    cin >> nombre;
     for (int i = 0; i < numPeliculas; i++) {
         if (peliculas[i].getNombre() == nombre) {
             for (int j = i; j < numPeliculas - 1; j++) {
@@ -52,31 +54,31 @@ void ordenarPeliculas() {
             }
         }
     }
-    std::cout << "Películas ordenadas exitosamente!\n";
+    cout << "Películas ordenadas exitosamente!\n";
 }
 
 void modificarPelicula() {
-    std::string nombre;
-    std::cout << "Ingrese el nombre de la película a modificar: ";
-    std::cin >> nombre;
+    string nombre;
+    cout << "Ingrese el nombre de la película a modificar: ";
+    cin >> nombre;
     for (int i = 0; i < numPeliculas; i++) {
         if (peliculas[i].getNombre() == nombre) {
-            std::string nuevoNombre, director, genero;
+            string nuevoNombre, director, genero;
             int anio, horas, minutos, segundos;
-            std::cout << "Ingrese el nuevo nombre de la película: ";
-            std::cin >> nuevoNombre;
+            cout << "Ingrese el nuevo nombre de la película: ";
+            cin >> nuevoNombre;
             peliculas[i].setNombre(nuevoNombre);
-            std::cout << "Ingrese el nuevo director de la película: ";
-            std::cin >> director;
+            cout << "Ingrese el nuevo director de la película: ";
+            cin >> director;
             peliculas[i].setDirector(director);
-            std::cout << "Ingrese el nuevo año de lanzamiento de la película: ";
-            std::cin >> anio;
+            cout << "Ingrese el nuevo año de lanzamiento de la película: ";
+            cin >> anio;
             peliculas[i].setAnioLanzamiento(anio);
-            std::cout << "Ingrese el nuevo género de la película: ";
-            std::cin >> genero;
+            cout << "Ingrese el nuevo género de la película: ";
+            cin >> genero;
             peliculas[i].setGenero(genero);
-            std::cout << "Ingrese la nueva duración de la película (horas, minutos, segundos): ";
-            std::cin >> horas >> minutos >> segundos;
+            cout << "Ingrese la nueva duración de la película (horas, minutos, segundos): ";
+            cin >> horas >> minutos >> segundos;
             Duracion duracion(horas, minutos, segundos);
             peliculas[i].setDuracion(duracion);
             break;
@@ -87,11 +89,11 @@ void modificarPelicula() {
 
 void imprimirPeliculas() {
     for (int i = 0; i < numPeliculas; i++) {
-        std::cout << "Nombre: " << peliculas[i].getNombre() << std::endl;
-        std::cout << "Director: " << peliculas[i].getDirector() << std::endl;
-        std::cout << "Año de Lanzamiento: " << peliculas[i].getAnioLanzamiento() << std::endl;
-        std::cout << "Genero: " << peliculas[i].getGenero() << std::endl;
-        std::cout << "Duración: " << peliculas[i].getDuracion().getHoras() << ":" << peliculas[i].getDuracion().getMinutos() << ":" << peliculas[i].getDuracion().getSegundos() << std::endl;
+        cout << "Nombre: " << peliculas[i].getNombre() << endl;
+        cout << "Director: " << peliculas[i].getDirector() << endl;
+        cout << "Año de Lanzamiento: " << peliculas[i].getAnioLanzamiento() << endl;
+        cout << "Genero: " << peliculas[i].getGenero() << endl;
+        cout << "Duración: " << peliculas[i].getDuracion().getHoras() << ":" << peliculas[i].getDuracion().getMinutos() << ":" << peliculas[i].getDuracion().getSegundos() << std::endl;
     }
 }
 
@@ -110,14 +112,14 @@ void guardarPeliculasEnArchivo() {
 }
 
 void cargarPeliculasDeArchivo() {
-    std::ifstream archivo("peliculas.txt");
-    std::string nombre, director, genero;
+    ifstream archivo("peliculas.txt");
+    string nombre, director, genero;
     int anio, horas, minutos, segundos;
-    while (std::getline(archivo, nombre)) {
-        std::getline(archivo, director);
+    while (getline(archivo, nombre)) {
+        getline(archivo, director);
         archivo >> anio;
         archivo.ignore();
-        std::getline(archivo, genero);
+        getline(archivo, genero);
         archivo >> horas;
         archivo.ignore();
         archivo >> minutos;
@@ -136,17 +138,17 @@ int problema5() {
     // Menú principal
     int opcion;
     do {
-        std::cout << "\nMENU PRINCIPAL\n";
-        std::cout << "1. Ingresar nueva película\n";
-        std::cout << "2. Eliminar película\n";
-        std::cout << "3. Ordenar películas\n";
-        std::cout << "4. Modificar película\n";
-        std::cout << "5. Imprimir lista de películas\n";
-        std::cout << "6. Guardar películas en archivo\n";
-        std::cout << "7. Cargar películas de archivo\n";
-        std::cout << "8. Salir\n";
-        std::cout << "Su opción: ";
-        std::cin >> opcion;
+        cout << "\nMENU PRINCIPAL\n";
+        cout << "1. Ingresar nueva película\n";
+        cout << "2. Eliminar película\n";
+        cout << "3. Ordenar películas\n";
+        cout << "4. Modificar película\n";
+        cout << "5. Imprimir lista de películas\n";
+        cout << "6. Guardar películas en archivo\n";
+        cout << "7. Cargar películas de archivo\n";
+        cout << "8. Salir\n";
+        cout << "Su opción: ";
+        cin >> opcion;
 
         switch (opcion) {
             case 1: // Ingresar nueva película
@@ -171,13 +173,14 @@ int problema5() {
                 cargarPeliculasDeArchivo();
                 break;
             case 8: // Salir
-                std::cout << "Saliendo del programa...\n";
+                cout << "Saliendo del programa...\n";
                 break;
             default:
-                std::cout << "Opción no valida. Intente nuevamente.\n";
+                cout << "Opción no valida. Intente nuevamente.\n";
         }
     } while (opcion != 8);
 
     return 0;
 }
+
 
